9472d1fb7056a881707b8e2eba4849407107be6b
Yao-Yin <Yao-Yin1997@users.noreply.github.com>
2020-05-07 17:02:36 +0900

    --- Auto-Git Commit ---

---
+++ Chapter_1/ant.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <cstdlib>
+#include <cstring>
+#include <climits>
+#include <algorithm>
+#include <numeric>
+#include <utility>
+#include <random>
+#include <chrono>
+#include <string>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+//const int mod = 1e9+7;
+int t;
+inline void quickread() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+}
+
+int main()
+{
+    quickread();
+    return 0;
+}
