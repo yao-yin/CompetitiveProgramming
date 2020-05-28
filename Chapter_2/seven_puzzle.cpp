a33bce408122939ce9ba4a876b3baa4d4e099ce9
Yao-Yin <Yao-Yin1997@users.noreply.github.com>
2020-05-28 15:56:33 +0900

    --- Auto-Git Commit ---

---
+++ Chapter_2/seven_puzzle.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <cstdlib>
+#include <cstring>
+#include <climits>
+#include <algorithm>
+#include <numeric>
+#include <utility>
+#include <random>
+#include <chrono>
+#include <string>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+//const int mod = 1e9+7;
+unordered_map<string, int> dist;
+int arr[2][4] = {0, 1, 2, 3, 4, 5, 6, 7};
+inline void quickread() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+}
+
+void init() {
+    
+}
+
+string arrToString() {
+    string res;
+    for(int i = 0; i < 2; i ++) {
+        for(int j = 0; j < 4; j ++) {
+            res += (arr[i][j] + '0');
+        }
+    }
+    return res;
+}
+int main()
+{
+    quickread();
+    init();
+    while(cin >> arr[0][0] >> arr[0][1] >> arr[0][2] >> arr[0][3] >> arr[1][0] >> arr[1][1] >> arr[1][2] >> arr[1][3]) {
+        cout << dist[arrToString()] << endl;
+    }
+    return 0;
+}
