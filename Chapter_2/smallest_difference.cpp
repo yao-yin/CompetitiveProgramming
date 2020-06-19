d512075ac2f944c3037065396d75e4978efa88b7
Yao-Yin <Yao-Yin1997@users.noreply.github.com>
2020-06-19 11:29:08 +0900

    --- Auto-Git Commit ---

---
+++ Chapter_2/smallest_difference.cpp
@@ -0,0 +1,83 @@
+#ifdef _MSC_VER
+#include <hash_map>
+#include <hash_set>
+using namespace stdext;
+#define unordered_map hash_map
+#define unordered_set hash_set
+#else
+#include <unordered_map>
+#include <unordered_set>
+#endif
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <cstdlib>
+#include <cstring>
+#include <climits>
+#include <algorithm>
+#include <numeric>
+#include <utility>
+#include <string>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <set>
+#include <map>
+
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+//const int mod = 1e9+7;
+const int N = 11;
+int nums[N];
+int test_case, n, diff;
+int form[N];
+int latter[N];
+const int INF = 0x3f3f3f3f;
+inline void quickread() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+}
+
+void getdiff() {
+    memset(form,0, sizeof form);
+    memset(latter,0, sizeof latter);
+    int f = 0;
+    for(int i = 0; i < n; i ++) {
+        form[i] = f;
+        f*= 10;
+        f += nums[i];
+    }
+    int base = 1;
+    for(int i = n-1 ; i > 0; i ++) {
+        latter[i] = latter[i+1] + base*nums[i];
+        base *= 10;
+    }
+    for(int i = 0; i < n-1; i ++) {
+        if(i > 0 && nums[0] == 0) continue;
+        if(i < n - 1 && nums[i+1] == 0) continue;
+        diff = min(diff, abs(form[i] - latter[i+1]));
+    }
+}
+
+void solve() {
+    memset(nums, 0, sizeof nums);
+    cin >> n;
+    diff = INF;
+    for(int i = 0; i < n; i ++) cin >> nums[i];
+    sort(nums, nums + n);
+    do {
+        getdiff();
+    } while(next_permutation(nums, nums + n));
+    cout << diff << endl;
+}
+
+int main()
+{
+    quickread();
+    cin >> test_case;
+    while(test_case --) {
+        solve();
+    }
+    return 0;
+}
