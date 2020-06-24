01b43492bb8592e5c3b784fa0e436962d8d97275
Yao-Yin <Yao-Yin1997@users.noreply.github.com>
2020-06-24 16:40:40 +0900

    --- Auto-Git Commit ---

--- Chapter_2/hopscotch.cpp
+++ Chapter_2/hopscotch.cpp
@@ -33,21 +33,44 @@ unordered_set<string> cache;
 char maze[5][5];
 int dirx[4];
 int diry[4];
-void dinit() {
+
+inline void dinit() {
     dirx[0] = 0, dirx[1] = -1, dirx[2] = 0, dirx[3] = 1;
-    diry[0] = -1, diry[1] = 0, dirx[2] = 1, dirx[3] = 0;
+    diry[0] = -1, diry[1] = 0, diry[2] = 1, diry[3] = 0;
 }
+
 inline void quickread() {
     ios::sync_with_stdio(false);
     cin.tie(0);
 }
 
-void solve() {
-    for(int i = 0; i < 5; i ++) {
-        for(int j = 0; j < 5; j ++) {
-            dfs()
+void dfs(int x, int y, int idx, string & curr) {
+    curr.push_back(maze[x][y]);
+    if(idx == 5) {
+        cache.insert(curr);
+        return;
+    } else {
+        int nx, ny;
+        for(int i = 0; i < 4; i ++) {
+            nx = x + dirx[i];
+            ny = y + diry[i];
+            if(nx < 0 || nx >= 5 || ny < 0 || ny >= 5) continue;
+            dfs(nx, ny, idx+1, curr);
+            curr.erase(curr.size()-1);
         }
     }
+}
+
+void solve() {
+    dinit();
+    string res;
+    for(int i = 0; i < 5; i ++) {
+        for(int j = 0; j < 5; j ++) {
+            res = "";
+            dfs(i, j, 0, res);
+        }
+    }
+    cout << cache.size() << endl;
 }
 int main() {
     quickread();
