1be3f3161ec262639b41e3bb5d0182173a9179da
Yao-Yin <Yao-Yin1997@users.noreply.github.com>
2020-07-03 16:57:41 +0900

    --- Auto-Git Commit ---

---
+++ Chapter_2/chapter2.2/Packets.cpp
@@ -0,0 +1,54 @@
+#ifdef _MSC_VER
+#include <hash_map>
+#include <hash_set>
+using namespace stdext;
+#define unordered_map hash_map
+#define unordered_set hash_set
+#else
+#include <unordered_map>
+#include <unordered_set>
+#endif
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <cstdlib>
+#include <cstring>
+#include <climits>
+#include <algorithm>
+#include <numeric>
+#include <utility>
+#include <string>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <set>
+#include <map>
+
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+//const int mod = 1e9+7;
+const int N = 7;
+ll nums[N];
+
+inline void quickread() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+}
+
+void solve() {
+    int res = 0;
+    res += nums[6];
+    res += nums[5];
+    nums[1] = max(0, nums[1] - 11*nums[5]);
+    res += nums[4];
+    nums[2] = max(0, nums[2] - 5*nums[4]);
+}
+
+int main() {
+    quickread();
+    while(cin >> nums[1] >> nums[2] >> nums[3] >> nums[4] >> nums[5] >> nums[6]) {
+        solve();
+    }
+    return 0;
+}
