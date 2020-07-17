ca51a1d0c769ec6c01146e427d69f2753beb1910
Yao-Yin <Yao-Yin1997@users.noreply.github.com>
2020-07-17 11:51:44 +0900

    --- Auto-Git Commit ---

--- Chapter_2/chapter2.3/Apple Catching.cpp
+++ Chapter_2/chapter2.3/Apple Catching.cpp
@@ -32,29 +32,36 @@ const int T = 1010;
 const int W = 33;
 int dp[T][W][3], w, t;
 int apples[T];
-inline void quickread() 
-{
+inline void quickread() {
     ios::sync_with_stdio(false);
     cin.tie(0);
 }
 
-int main() 
-{
+int main() {
     quickread();
-    cin >> w >> t;
+    cin >> t >> w;
     memset(dp, -0x3f3f3f3f, sizeof dp);
     dp[0][0][1] = 0;
-    for (int i = 1; i <= t; i ++) 
-    {
+    for (int i = 1; i <= t; i ++) {
         cin >> apples[i];
     }
-    for (int i = 1; i <= t; i ++) 
-    {
-        dp[]
-        for (int j = 1; j <= w; j ++)
-        {
-            dp[i][j][1] = max(dp[i][j-1])
+    for (int i = 1; i <= t; i ++) {
+        for (int j = 0; j <= w; j ++) {
+            dp[i][j][1] = dp[i-1][j][1];
+            dp[i][j][2] = dp[i-1][j][2];
+            if (j > 0) {
+                dp[i][j][1] = max(dp[i][j][1], dp[i-1][j-1][2]);
+                dp[i][j][2] = max(dp[i][j][2], dp[i-1][j-1][1]);
+            }
+            dp[i][j][apples[i]] ++;
         }
     }
+    int res = 0;
+    for (int i = 0; i <= w; i ++) {
+        for (int j = 1; j <= 2; j ++) {
+            res = max(res, dp[t][i][j]);
+        }
+    }
+    cout << res << endl;
     return 0;
 }
