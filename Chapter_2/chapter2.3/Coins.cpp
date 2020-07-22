851b0ce4038d1ea83daf68bfab442ecbf2a27a39
Yao-Yin <Yao-Yin1997@users.noreply.github.com>
2020-07-22 22:52:29 +0900

    --- Auto-Git Commit ---

--- Chapter_2/chapter2.3/Coins.cpp
+++ Chapter_2/chapter2.3/Coins.cpp
@@ -29,7 +29,9 @@ typedef long long ll;
 typedef pair<int, int> pii;
 //const int mod = 1e9+7;
 const int N = 110;
-const int M = 100010
+const int M = 100010;
+int a[N], c[N], n, m;
+int dp[M];
 
 inline void quickread() {
     ios::sync_with_stdio(false);
@@ -38,6 +40,23 @@ inline void quickread() {
 
 int main() {
     quickread();
-    
+    while (cin >> n >> m, n || m) {
+        memset(dp, -1, sizeof dp);
+        dp[0] = 0;
+        for (int i = 1; i <= n; i ++) cin >> a[i];
+        for (int i = 1; i <= n; i ++) cin >> c[i];
+        for (int i = 1; i <= n; i ++) {
+            for (int j = 0; j <= m; j ++) {
+                if (dp[j] >= 0) dp[j] = c[i];
+                else if (j < a[i] || dp[j-a[i]] <= 0) dp[j] = -1;
+                else dp[j] = dp[j-a[i]] -1;
+            }
+        }
+        int res = 0;
+        for (int i = 1; i <= m; i ++) {
+            if (dp[i] >= 0) res ++;
+        }
+        cout << res << endl;
+    }
     return 0;
 }
